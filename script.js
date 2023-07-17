bb7b08bf7ac9ae9e2f5c39a058649fa36ab3b9c0
simone <simone44c@gmail.com>
2023-07-17 16:58:09 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+"use strict";
+console.log('hello world');
+let controlResul = function () {
+    let num = parseFloat(document.querySelector("#firstN").value);
+    console.log(num);
+    let num2 = parseFloat(document.querySelector("#secondN").value);
+    console.log(num2);
+    let containerNumber = document.querySelector(".containerRandomNumber");
+    let randomNumber = Math.floor(Math.random() * 101);
+    console.log('il numero random è:', randomNumber);
+    let firstDifference = Math.abs(randomNumber - num);
+    console.log('firsstdifference è:', firstDifference);
+    let secondDifference = Math.abs(randomNumber - num2);
+    console.log('secondDifference è:', secondDifference);
+    if (num === randomNumber) {
+        console.log('firstNumber ha indovinato');
+        containerNumber.innerHTML = 'il numero estrtto è : ' + randomNumber + ' firstNumber ha indovinato';
+    }
+    else if (num2 === randomNumber) {
+        console.log('secondNumber ha indovinato');
+        containerNumber.innerHTML = 'il numero estrtto è :' + randomNumber + 'secondNumber ha indovinato';
+    }
+    else if (firstDifference < secondDifference) {
+        console.log('firstNumber si è avvicinato di piu');
+        containerNumber.innerHTML = 'il numero estrtto è: ' + randomNumber + ' firstNumber si è avvicinato di piu';
+    }
+    else {
+        console.log('secondNumber si è avvicinato di piu');
+        containerNumber.innerHTML = 'il numero estratto è: ' + randomNumber + ' secondNumber si è avvicinato di piu';
+    }
+};
+let myForm = document.querySelector('form');
+console.log(myForm);
+myForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    controlResul();
+});
